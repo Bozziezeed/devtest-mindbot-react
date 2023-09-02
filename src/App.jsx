e6db23478427dc029b80ea6652ac92141eb7f0d5
Bozziezeed <netiwit.ph@gmail.com>
2023-09-02 09:46:44 +0700

    first commit file

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import GenderGuessingApp from "./components/GenderGuessingApp";
+function App() {
+  return (
+    <>
+      <GenderGuessingApp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/GenderCard.jsx
@@ -0,0 +1,50 @@
+import femaleImage from "../assets/female.png";
+import maleImage from "../assets/male.png";
+import "../style/GenderCard.css";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+function GenderCard({ result }) {
+  let genderImage;
+  if (result.name.length > 0) {
+    genderImage = result.gender === "female" ? femaleImage : maleImage;
+  }
+  return result.probability > 0.1 ? (
+    <div className="card-container">
+      <div className="image-bg-circle">
+        <img src={genderImage} alt={genderImage} />
+      </div>
+      <div className="result-card">
+        <p className="name">{result.name}</p>
+        <div className="rectangle">
+          <div className="percent">
+            <span>
+              <span className="percent-span">✦ </span>
+              <span className="percent-span2">Gender:</span>
+              <span className="percent-span3"> </span>
+              <span className={`percent-span4 ${result.gender}`}>
+                {result.gender.toUpperCase()}
+                <br />
+              </span>
+              <span className="percent-span5">✦ </span>
+              <span className="percent-span6">Probability:</span>
+              <span className="percent-span7"> </span>
+              <span className={`percent-span8 ${result.gender}`}>
+                {result.probability * 100}%
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="card-container">
+      <div className="name-error">
+        Can not generate gender
+        <br />
+        Please try other name
+      </div>
+    </div>
+  );
+}
+
+export default GenderCard;
